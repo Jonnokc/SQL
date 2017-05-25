bb7fcbb82764d5b3882e4f09887e27d7733c1d96
Jonnokc <jonnoadams@gmail.com>
2017-05-25 12:57:23 -0500

    Adjusted layout

--- 60-Baseline_Measure_Report.sql
+++ 60-Baseline_Measure_Report.sql
@@ -1,50 +1,34 @@
 --Replace <client> with client mnemonic
 --Replace <POP_ID> with population id
-SELECT fully_qualified_name
-	,count(CASE
-			WHEN STATE = 'MET'
-				THEN empi_id
-			ELSE NULL
-			END) AS persons_met
-	,(
-		count(CASE
-				WHEN STATE = 'MET'
-					THEN empi_id
-				ELSE NULL
-				END) + count(CASE
-				WHEN STATE = 'NOT_MET_DATA_AVAILABLE'
-					THEN empi_id
-				ELSE NULL
-				END) + count(CASE
-				WHEN STATE = 'NOT_MET_NO_DATA'
-					THEN empi_id
-				ELSE NULL
-				END)
-		) AS persons_included
-	,count(CASE
-			WHEN STATE = 'MET'
-				THEN empi_id
-			ELSE NULL
-			END) / (
-		(
-			count(CASE
-					WHEN STATE = 'MET'
-						THEN empi_id
-					ELSE NULL
-					END) + count(CASE
-					WHEN STATE = 'NOT_MET_DATA_AVAILABLE'
-						THEN empi_id
-					ELSE NULL
-					END) + count(CASE
-					WHEN STATE = 'NOT_MET_NO_DATA'
-						THEN empi_id
-					ELSE NULL
-					END)
-			)
-		) AS met_percentage
 
+SELECT fully_qualified_name,
+       count(CASE
+                 WHEN STATE = 'MET' THEN empi_id
+                 ELSE NULL
+             END) AS persons_met,
+       (count(CASE
+                  WHEN STATE = 'MET' THEN empi_id
+                  ELSE NULL
+              END) + count(CASE
+                               WHEN STATE = 'NOT_MET_DATA_AVAILABLE' THEN empi_id
+                               ELSE NULL
+                           END) + count(CASE
+                                            WHEN STATE = 'NOT_MET_NO_DATA' THEN empi_id
+                                            ELSE NULL
+                                        END)) AS persons_included,
+       count(CASE
+                 WHEN STATE = 'MET' THEN empi_id
+                 ELSE NULL
+             END) / ((count(CASE
+                                WHEN STATE = 'MET' THEN empi_id
+                                ELSE NULL
+                            END) + count(CASE
+                                             WHEN STATE = 'NOT_MET_DATA_AVAILABLE' THEN empi_id
+                                             ELSE NULL
+                                         END) + count(CASE
+                                                          WHEN STATE = 'NOT_MET_NO_DATA' THEN empi_id
+                                                          ELSE NULL
+                                                      END))) AS met_percentage
 FROM <client>.ph_f_measure_outcome
-
 WHERE population_id = '<POP_ID>'
-
 GROUP BY fully_qualified_name
